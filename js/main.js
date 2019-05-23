eb8ee502f4fbee212ff5097fe232cc8faaba79a9
avicndugu <avicnduguh@gmail.com>
2019-05-23 13:40:31 +0300

    Time & date format set and showing on DOM correctly.

--- js/main.js
+++ js/main.js
@@ -2,24 +2,49 @@
 var nowDateTime= new Date();
 console.log (nowDateTime);
 var day=nowDateTime.getDate();
-var month=nowDateTime.getMonth();
+var month=nowDateTime.toLocaleString('en-us', { month: 'long' });
 var year=nowDateTime.getFullYear();
 var hour= nowDateTime.getHours();
 var minute= nowDateTime.getMinutes();
+var twelveHour;
+
+// DOM INSERTION VARIABLES
+const hours=document.getElementById("hours");
+const minutes=document.getElementById("minutes");
+const amPm=document.getElementById("am-pm");
+const viewDay=document.getElementById("day");
+const viewMonth=document.getElementById("month");
+const viewYear=document.getElementById("year");
 
 // Choose if AM or PM
-if (hour>12) {
-    console.log("PM");
+if (hour>=12) {
+    if (minute==0){
+        twelveHour="PM";        
+    }
+    else{
+        twelveHour="PM";
+        console.log(twelveHour);
+    }    
 }
 else {
-    console.log("AM");
+    twelveHour="AM";
+    console.log(twelveHour);
+
 }
 
 // Convert time from 24 hours to 12 hour system
 if (hour>12){
     hour=hour-12;
-    console.log(hour);
 }
-else {
-    console.log(hour);
+
+// Adds a zero if minutes are below 10
+if (minute<10){
+    minute="0"+minute;
 }
+
+minutes.innerText=minute;
+hours.innerText=hour;
+amPm.innerText=twelveHour;
+viewDay.innerText=day;
+viewMonth.innerText=month+" ";
+viewYear.innerText=" "+year;
